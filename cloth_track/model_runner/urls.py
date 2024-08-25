ce4dd62dbc5a04cf4af34e27b92c7c3fdad95696
Talha Javed Mukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2024-08-25 22:16:21 +0500

    more bugs fixed

--- cloth_track/model_runner/urls.py
+++ cloth_track/model_runner/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('run-sam2/', views.RunSam2View.as_view(), name='run_sam2'),
-    path('run-yolo/', views.run_yolo, name='run_yolo'),
+    path('run-yolo/', views.RunYOLOView.as_view(), name='run_yolo'),
     path('test/', views.DoubleNumberView.as_view(), name='double_number'),
 ]
