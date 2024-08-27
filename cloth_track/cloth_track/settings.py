295b86bab43ee8406407121992a9900adf70da70
Talha Javed Mukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2024-08-27 17:31:40 +0500

    changed host settings on the django project

--- cloth_track/cloth_track/settings.py
+++ cloth_track/cloth_track/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-w=ox#+q0dn4(vjsb2smc53kz7fk6kudlqi7c11bfzjju+xha=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
