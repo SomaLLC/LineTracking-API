a8635049ebe96d185c7e4e80519a805c06a78987
talhajavedmukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2024-09-09 15:16:26 +0500

    fix file upload sizes

--- cloth_track/cloth_track/settings.py
+++ cloth_track/cloth_track/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 1000 * 1024 * 1024  # 50 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 1000 * 1024 * 1024  # 50 MB
+
