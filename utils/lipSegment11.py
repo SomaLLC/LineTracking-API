fd29bf632342b9d2d6fb0756879a5f65a28897e9
talhajavedmukhtar <tjaved.bscs15seecs@seecs.edu.pk>
2024-09-10 23:57:52 +0500

    typo fix 5

--- utils/lipSegment11.py
+++ utils/lipSegment11.py
@@ -118,7 +118,7 @@ def detect_cat_face(frame):
     top = min(y_coords)
     bottom = max(y_coords)
 
-    return (left, top, right - left, top - bottom)
+    return (left, top, min(1,right - left), min(1, bottom - top))
 
 
 def detect_cat_nose(frame, face_rect):
